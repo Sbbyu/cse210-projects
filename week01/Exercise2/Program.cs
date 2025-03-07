fa998ba4a9e740e571b2c6bc3a1efe0399453d82
Sbbyu <stacie9801@gmail.com>
2025-03-06 21:53:03 -0700

    wrote code to tell user what letter grade they got and tell them if they passed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("What is your grade percentage? ");
+        int userScore = int.Parse(Console.ReadLine());
+        
+        string letter = "empty";
+
+        if (userScore >= 90)
+        {
+            letter = "A";
+           
+        }
+
+        else if (90 > userScore && userScore >= 80)
+        {
+            letter = "B";
+           
+        }  
+
+        else if (80 > userScore && userScore >= 70)
+        {
+            letter = "C";
+           
+        }    
+
+        else if (70 > userScore && userScore >= 60)
+        {
+            letter = "D";
+            
+        }  
+
+        else if (userScore < 60)
+        {
+            letter = "F";
+            
+        }
+
+        else
+        {
+            Console.WriteLine("Error");
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (userScore >= 70)
+        {
+            Console.WriteLine("Yay you passed!!!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass, You can try again!");
+        }
+
     }
 }

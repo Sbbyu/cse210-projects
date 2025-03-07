0fe9e528e71b1ab8b53b3b9689467ba09f4e97bb
Sbbyu <stacie9801@gmail.com>
2025-03-06 20:27:45 -0700

    completed code to ask for users name and make it sound bondlike

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        //Asks user for first name and stores it as a string varible
+        Console.WriteLine("What is your first name? ");
+        string firstname = Console.ReadLine();
+
+        //Asks user for last name and stores it as a string varible
+        Console.WriteLine("What is your last name? ");
+        string lastname = Console.ReadLine();
+
+
+        //prints the users name in a message
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
+
+
+
+
     }
 }
